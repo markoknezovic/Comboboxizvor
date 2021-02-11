7d6dcb05f670bfc5032772639e8910f369e67463
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-02-11 13:15:37 +0100

    Add files via upload

---
+++ 00ComboBox/00ComboBox/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace _00ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBoxIzbor = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 289);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBoxIzbor
+            // 
+            this.comboBoxIzbor.FormattingEnabled = true;
+            this.comboBoxIzbor.Items.AddRange(new object[] {
+            "Ormar",
+            "Cekic",
+            "Drvo",
+            "Klima"});
+            this.comboBoxIzbor.Location = new System.Drawing.Point(104, 210);
+            this.comboBoxIzbor.Name = "comboBoxIzbor";
+            this.comboBoxIzbor.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxIzbor.TabIndex = 1;
+            this.comboBoxIzbor.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxIzbor);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBoxIzbor;
+    }
+}
+
